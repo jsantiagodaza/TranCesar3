f197b26c590956cd51965d44ce50c398728fd0ca
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-18 22:30:07 -0500

    feat: finalizacionde clase vehiculo con getter, stters y metodos

--- src/Modelo/Vehiculo.java
+++ src/Modelo/Vehiculo.java
@@ -23,7 +23,66 @@ public abstract class Vehiculo implements Imprimible {
         this.disponible = true;
     }
 
+     public boolean tieneCupos() {
+        return pasajerosActuales < capacidadMaxima;
+    }
+
+    public int getCuposDisponibles() {
+        return capacidadMaxima - pasajerosActuales;
+    }
+
+    public void agregarPasajero() {
+        if (tieneCupos()) pasajerosActuales++;
+    }
+    
+    
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public int getPasajerosActuales() {
+        return pasajerosActuales;
+    }
+
+    public void setPasajerosActuales(int pasajerosActuales) {
+        this.pasajerosActuales = pasajerosActuales;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public abstract String getTipo();
+
+    public String toCSV() {
+        return placa + ";" + ruta + ";" + pasajerosActuales + ";" + (disponible ? "1" : "0");
+    }
+}
+
   
     
-    
-}
+   
