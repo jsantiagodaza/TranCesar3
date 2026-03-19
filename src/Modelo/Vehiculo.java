b286274763cb29744f95e71df14b56e522e7ff95
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-18 22:24:56 -0500

    feat: creacion de clase Vehiculo y su constructor

--- src/Modelo/Vehiculo.java
+++ src/Modelo/Vehiculo.java
@@ -8,6 +8,22 @@ package Modelo;
  *
  * @author 2jcue
  */
-public class Vehiculo {
+public abstract class Vehiculo implements Imprimible {
+    protected String placa;
+    protected String ruta;
+    protected int capacidadMaxima;
+    protected int pasajerosActuales;
+    protected boolean disponible;
+    protected double tarifaBase;  
+
+    public Vehiculo(String placa, String ruta) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.pasajerosActuales = 0;
+        this.disponible = true;
+    }
+
+  
+    
     
 }
