9773b4ea061022bb6c4ac96814dcfba68ac83721
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 16:00:42 -0500

    feat: Crear clase TicketService con atributos

---
+++ src/Logica/TicketService.java
@@ -0,0 +1,61 @@
+package Logica;
+
+import Persistencia.TicketDAO;
+import Modelo.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketService {
+
+    private TicketDAO dao;
+    private List<Ticket> tickets;
+    private VehiculoService vehiculoService;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
