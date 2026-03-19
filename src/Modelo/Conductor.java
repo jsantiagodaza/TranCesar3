0a38325ab0fd08ddcd022ed8dea52b5292851ed5
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-19 16:09:31 -0500

    feat:creacion de la clse conductor

---
+++ src/Modelo/Conductor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author 2jcue
+ */
+public class Conductor extends Persona {
+    private String numeroLicencia;
+    private String categoriaLicencia;
+
+    public Conductor(String cedula, String nombre, String numeroLicencia, String categoriaLicencia) {
+        super(cedula, nombre);
+        this.numeroLicencia = numeroLicencia;
+        this.categoriaLicencia = categoriaLicencia;
+    }
+}
