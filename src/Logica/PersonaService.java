f54c6dabc14970d66a9d93fd5bdaf8168eca01aa
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 15:42:33 -0500

    feat: Crear clase PersonaService con atributos DAO y listas

---
+++ src/Logica/PersonaService.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import Persistencia.ConductorDAO;
+import Persistencia.PasajeroDAO;
+import Modelo.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaService {
+
+    private ConductorDAO conductorDAO;
+    private PasajeroDAO  pasajeroDAO;
+    private List<Conductor> conductores;
+    private List<Pasajero>  pasajeros;
+    
+}
