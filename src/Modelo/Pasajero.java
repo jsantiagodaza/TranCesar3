5d58e6c2322bc4ccf2b341decbc991e785c4afa5
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 15:19:04 -0500

    feat: Metodo de descuento en la clase Pasajero

--- src/Modelo/Pasajero.java
+++ src/Modelo/Pasajero.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author PC
- */
-public class Pasajero {
-    
+
+public abstract class Pasajero extends Persona {
+    protected String tipo;
+
+    public Pasajero(String cedula, String nombre, String tipo) {
+        super(cedula, nombre);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() { return tipo; }
+
+    public abstract double calcularDescuento();
+
+    public String toCSV() {
+        return cedula + ";" + nombre + ";" + tipo;
+    }
 }
