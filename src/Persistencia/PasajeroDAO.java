11d8dc2c8c26cd979755169685e353502a14c21f
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 16:09:35 -0500

    feat: Crear clase PasajeroDAO con constante ARCHIVO

---
+++ src/Persistencia/PasajeroDAO.java
@@ -0,0 +1,13 @@
+package Persistencia;
+
+
+import Presentacion.*;
+import Modelo.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PasajeroDAO {
+
+    private static final String ARCHIVO = "pasajeros.txt";
+}
