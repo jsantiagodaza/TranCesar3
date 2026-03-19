18f915f85709abf85e9f4187efc561234313b437
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 15:35:59 -0500

    feat: Agregar constructores a Ticket (nuevo y desde archivo)

--- src/Modelo/Ticket.java
+++ src/Modelo/Ticket.java
@@ -11,4 +11,56 @@ public class Ticket implements Imprimible, Calculable {
     private String origen;
     private String destino;
     private double valorFinal;
+    
+    
+    
+    public Ticket(Pasajero pasajero, Vehiculo vehiculo, String origen, String destino) {
+    this.id = contadorId++;
+    this.pasajero = pasajero;
+    this.vehiculo = vehiculo;
+    this.fechaCompra = LocalDate.now();
+    this.origen = origen;
+    this.destino = destino;
+    this.valorFinal = calcularTotal();
 }
+
+// Constructor para cargar desde archivo
+public Ticket(int id, Pasajero pasajero, Vehiculo vehiculo,
+              LocalDate fechaCompra, String origen, String destino, double valorFinal) {
+    this.id = id;
+    this.pasajero = pasajero;
+    this.vehiculo = vehiculo;
+    this.fechaCompra = fechaCompra;
+    this.origen = origen;
+    this.destino = destino;
+    this.valorFinal = valorFinal;
+    if (id >= contadorId) contadorId = id + 1;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
