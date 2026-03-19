2dfc423c115fd2e2bad2d4d234cfc490161fcaf9
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-19 11:29:00 -0500

    feat:creacion de los constuctores de service

--- src/Logica/VehiculoService.java
+++ src/Logica/VehiculoService.java
@@ -8,6 +8,18 @@ package Logica;
  *
  * @author 2jcue
  */
+import Persistencia.VehiculoDAO;
+import Modelo.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehiculoService {
-    
+
+    private VehiculoDAO dao;
+    private List<Vehiculo> vehiculos;
+
+    public VehiculoService() {
+        this.dao = new VehiculoDAO();
+        this.vehiculos = dao.cargarTodos();
+    }
 }
