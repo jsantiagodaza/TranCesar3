a6adff3f44785d63fecacf079eccd14f6f25e828
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-18 21:42:55 -0500

    feat: se creó una clase que no tendrá uso, solo se hizo para inicilizar el paquete

---
+++ src/Logica/eliminable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author santi
+ */
+public class eliminable {
+    //esta clase solo existe para inicializar el paquete, será borrada pronto.
+}
