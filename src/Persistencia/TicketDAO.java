4e70a3564fff435cf3f02bb99b13938e7cde9306
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 16:18:30 -0500

    feat: Crear clase TicketDAO con constante ARCHIVO

---
+++ src/Persistencia/TicketDAO.java
@@ -0,0 +1,16 @@
+package Persistencia;
+
+import Presentacion.*;
+import Modelo.*;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketDAO {
+
+    private static final String ARCHIVO = "tickets.txt";
+    
+    
+    
+}
