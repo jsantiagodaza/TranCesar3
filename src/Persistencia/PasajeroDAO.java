8424f7d4e2643d25eba632a695801fbbc952b8a3
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-19 16:10:35 -0500

    feat: Implementar método guardar en PasajeroDAO

--- src/Persistencia/PasajeroDAO.java
+++ src/Persistencia/PasajeroDAO.java
@@ -10,4 +10,16 @@ import java.util.List;
 public class PasajeroDAO {
 
     private static final String ARCHIVO = "pasajeros.txt";
+    
+        public void guardar(Pasajero p) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO, true))) {
+            bw.write(p.toCSV());
+            bw.newLine();
+        } catch (IOException e) {
+            System.err.println("Error al guardar pasajero: " + e.getMessage());
+        }
+    }
+    
+    
+    
 }
