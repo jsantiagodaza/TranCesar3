6058443650416f31246aea40591feebb6c1f2b69
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-20 18:45:48 -0500

    feat: creacion de atribustos y constructor de clase ruta

--- src/Modelo/Ruta.java
+++ src/Modelo/Ruta.java
@@ -8,6 +8,21 @@ package Modelo;
  *
  * @author 2jcue
  */
-public class Ruta {
+   public class Ruta implements Imprimible {
+    private String codigo;
+    private String ciudadOrigen;
+    private String ciudadDestino;
+    private double distanciaKm;
+    private int tiempoEstimadoMinutos;  
+
+    public Ruta(String codigo, String ciudadOrigen, String ciudadDestino, double distanciaKm, int tiempoEstimadoMinutos) {
+        this.codigo = codigo;
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.distanciaKm = distanciaKm;
+        this.tiempoEstimadoMinutos = tiempoEstimadoMinutos;
+    }
+    
+    
     
 }
