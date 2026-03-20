21e9698b0eb18d47ad22455167bff1b37d6adf20
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-19 19:28:18 -0500

    feat: se actualizó el metodo mostrarMenu() en la clase PersonaView, ahora sí gestiona las opciones de PersonaView

--- src/Presentacion/PersonaView.java
+++ src/Presentacion/PersonaView.java
@@ -26,7 +26,31 @@ public class PersonaView {
         this.sc = sc;
     }
     public void mostrarMenu() {
-     
+      int opcion;
+        do {
+            System.out.println("\n╔===================================+");
+            System.out.println("|       GESTION DE PERSONAS         |");
+            System.out.println("+===================================+");
+            System.out.println("|  1. Registrar conductor           |");
+            System.out.println("|  2. Listar conductores            |");
+            System.out.println("|  3. Asignar conductor a vehiculo  |");
+            System.out.println("|  4. Registrar pasajero            |");
+            System.out.println("|  5. Listar pasajeros              |");
+            System.out.println("|  0. Volver                        |");
+            System.out.println("+===================================+");
+            System.out.print("  Opcion: ");
+            opcion = leerInt();
+
+            switch (opcion) {
+                case 1: registrarConductor();        break;
+                case 2: listarConductores();         break;
+                case 3: asignarConductorAVehiculo(); break;
+                case 4: registrarPasajero();         break;
+                case 5: listarPasajeros();           break;
+                case 0: break;
+                default: System.out.println("  Opcion invalida.");
+            }
+        } while (opcion != 0);
     }
     private void registrarConductor() {
         
