f5a7cac3e12607c61ca337656ced92b3f91c56cd
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-20 19:11:26 -0500

    feat: creacion de la funcion guardar en rutaDAO

--- src/Persistencia/RutaDAO.java
+++ src/Persistencia/RutaDAO.java
@@ -8,6 +8,21 @@ package Persistencia;
  *
  * @author 2jcue
  */
+import Modelo.Ruta;
+import java.io.*;
+
+
 public class RutaDAO {
+    
+    private static final String ARCHIVO = "rutas.txt";
+
+    public void guardar(Ruta r) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO, true))) {
+            bw.write(r.toCSV());
+            bw.newLine();
+        } catch (IOException e) {
+            System.err.println("Error al guardar ruta: " + e.getMessage());
+        }
+    }
     
 }
