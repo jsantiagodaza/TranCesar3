89aceb06cca6752c915c9426d7d9cfc645d90d3f
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-21 12:33:38 -0500

    feat:Creacion de atributos de la clase reserva

--- src/Modelo/Reserva.java
+++ src/Modelo/Reserva.java
@@ -8,6 +8,14 @@ package Modelo;
  *
  * @author 2jcue
  */
-public class Reserva {
+public class Reserva implements Imprimible {
+
+    private static int contadorId = 1;
+    private String codigo;
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaViaje;
+    private EstadoReserva estado;   
     
 }
