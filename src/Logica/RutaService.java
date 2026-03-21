f895842901dbd09b2e0e99a4486ea72d4dbf860f
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-20 20:24:06 -0500

    feat: Crear clase RutaService con atributos DAO y lista

---
+++ src/Logica/RutaService.java
@@ -0,0 +1,12 @@
+package Logica;
+
+import Persistencia.RutaDAO;
+import Modelo.Ruta;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RutaService {
+
+    private RutaDAO dao;
+    private List<Ruta> rutas;
+}
