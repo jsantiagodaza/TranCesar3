84aa1b6c015a136a6f0b0dc774dc235f6e0caf91
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-21 12:57:11 -0500

    feat: Crear clase ReservaService con atributos DAO, lista y servicios

---
+++ src/Logica/ReservaService.java
@@ -0,0 +1,20 @@
+package Logica;
+
+import Persistencia.ReservaDAO;
+import Modelo.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservaService {
+
+    private ReservaDAO dao;
+    private List<Reserva> reservas;
+    private VehiculoService vehiculoService;
+    private FestivoService festivoService;
+    
+    
+    
+    
+}
