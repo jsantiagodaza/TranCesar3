ab778d40a7679265c6baac79272ced27202a4e0a
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-20 20:34:20 -0500

    feat: Agregar festivos fijos de Colombia en bloque estático

--- src/Logica/FestivoService.java
+++ src/Logica/FestivoService.java
@@ -9,4 +9,16 @@ public class FestivoService {
 
     // Festivos fijos Colombia (día/mes)
     private static final Set<MonthDay> FESTIVOS_FIJOS = new HashSet<>();
+    
+    static {
+    FESTIVOS_FIJOS.add(MonthDay.of(1,  1));  // Año Nuevo
+    FESTIVOS_FIJOS.add(MonthDay.of(5,  1));  // Día del Trabajo
+    FESTIVOS_FIJOS.add(MonthDay.of(7,  20)); // Independencia
+    FESTIVOS_FIJOS.add(MonthDay.of(8,  7));  // Batalla de Boyacá
+    FESTIVOS_FIJOS.add(MonthDay.of(12, 8));  // Inmaculada Concepción
+    FESTIVOS_FIJOS.add(MonthDay.of(12, 25)); // Navidad
+        }
+    
+    
+    
 }
