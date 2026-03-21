f5a127b2c4f007d281723977f4bd547dcdbf141c
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-20 19:40:46 -0500

    feat: Cambios en el codigo de la clase PasajeroAdultoMayot

--- src/Modelo/PasajeroAdultoMayor.java
+++ src/Modelo/PasajeroAdultoMayor.java
@@ -1,9 +1,12 @@
 package Modelo;
 
+
+import java.time.LocalDate;
+
 public class PasajeroAdultoMayor extends Pasajero {
 
-    public PasajeroAdultoMayor(String cedula, String nombre) {
-        super(cedula, nombre, "AdultoMayor");
+    public PasajeroAdultoMayor(String cedula, String nombre, LocalDate fechaNacimiento) {
+        super(cedula, nombre, fechaNacimiento);
     }
 
     @Override
@@ -14,6 +17,7 @@ public class PasajeroAdultoMayor extends Pasajero {
         System.out.println("========== PASAJERO ==========");
         System.out.println("Cedula   : " + cedula);
         System.out.println("Nombre   : " + nombre);
+        System.out.println("Edad     : " + getEdad() + " años");
         System.out.println("Tipo     : Adulto Mayor");
         System.out.println("Descuento: 30%");
         System.out.println("==============================");
