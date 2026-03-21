a1c39aa885db464d7514bf5e00df0e90ed586ead
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-21 13:18:02 -0500

    feat: Crear clase ReservaDAO con constante ARCHIVO

---
+++ src/Persistencia/ReservaDAO.java
@@ -0,0 +1,50 @@
+package Persistencia;
+
+import Modelo.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservaDAO {
+
+    private static final String ARCHIVO = "reservas.txt";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
