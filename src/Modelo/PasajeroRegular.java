67a7e4ff0d5853207132463ffafbf50a349ea064
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-20 19:43:10 -0500

    feat: Cambios en el codigo de la clase PasajeroRegular

--- src/Modelo/PasajeroRegular.java
+++ src/Modelo/PasajeroRegular.java
@@ -1,10 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Modelo;
 
+/**
+ *
+ * @author 2jcue
+ */
+import java.time.LocalDate;
 
 public class PasajeroRegular extends Pasajero {
 
-    public PasajeroRegular(String cedula, String nombre) {
-        super(cedula, nombre, "Regular");
+    public PasajeroRegular(String cedula, String nombre, LocalDate fechaNacimiento) {
+        super(cedula, nombre, fechaNacimiento);
     }
 
     @Override
@@ -15,6 +24,7 @@ public class PasajeroRegular extends Pasajero {
         System.out.println("========== PASAJERO ==========");
         System.out.println("Cedula   : " + cedula);
         System.out.println("Nombre   : " + nombre);
+        System.out.println("Edad     : " + getEdad() + " años");
         System.out.println("Tipo     : Regular");
         System.out.println("Descuento: 0%");
         System.out.println("==============================");
