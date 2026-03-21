f9c31bc92b4a4a043054fd838dd7e69539e7e157
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-21 14:29:20 -0500

    feat: se creó la clase ReservaView en Presentación, se le hicieron sus metodos base.

---
+++ src/Presentacion/ReservaView.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Presentacion;
+
+/**
+ *
+ * @author santi
+ */
+public class ReservaView {
+
+    public ReservaView() {
+    }
+      public void mostrarMenu() {
+      }
+      public void crearReserva(){
+      }
+       private void listarActivas() {
+       }
+           private void historialPasajero() {
+           }
+               private void convertirEnTicket() {
+               }
+                   private void verificarVencidas() {
+                   }
+                       private int leerInt() {
+                       return 0;
+                       }
+}
