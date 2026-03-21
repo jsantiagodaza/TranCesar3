55f84b3ab97f2e63b2e6ad5a9e843212cec23820
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-21 13:19:15 -0500

    feat: Implementar método guardar en ReservaDAO

--- src/Persistencia/ReservaDAO.java
+++ src/Persistencia/ReservaDAO.java
@@ -11,6 +11,15 @@ public class ReservaDAO {
     private static final String ARCHIVO = "reservas.txt";
     
     
+    public void guardar(Reserva r) {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO, true))) {
+        bw.write(r.toCSV());
+        bw.newLine();
+    } catch (IOException e) {
+        System.err.println("Error al guardar reserva: " + e.getMessage());
+    }
+}
+    
     
     
     
