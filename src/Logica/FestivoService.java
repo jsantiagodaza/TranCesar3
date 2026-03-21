72266384e1897b7801a1c67e8125a31bf2a3bf3c
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-20 20:33:05 -0500

    feat: Crear clase FestivoService con Set de festivos fijos

---
+++ src/Logica/FestivoService.java
@@ -0,0 +1,12 @@
+package Logica;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FestivoService {
+
+    // Festivos fijos Colombia (día/mes)
+    private static final Set<MonthDay> FESTIVOS_FIJOS = new HashSet<>();
+}
